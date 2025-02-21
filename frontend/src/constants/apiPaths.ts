79db7b1bc637b375fb978bb1c1244187d5a3b1b9
Ihor Osadchyi <ihor_osadchyi@epam.com>
2025-02-21 21:34:07 +0200

    chore: refactor repo structure

---
+++ frontend/src/constants/apiPaths.ts
@@ -0,0 +1,9 @@
+const API_PATHS = {
+  product: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
+  order: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
+  import: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
+  cart: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
+};
+
+export default API_PATHS;
