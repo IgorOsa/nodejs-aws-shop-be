92b7095e2c07858ccdde75e52cf3790812e63f08
Ihor Osadchyi <ihor_osadchyi@epam.com>
2025-02-22 13:04:14 +0200

    feat: add swagger

--- backend/product-service/get-products-by-id.ts
+++ backend/product-service/get-products-by-id.ts
@@ -1,6 +1,50 @@
 import { httpResponse } from "./common/http-responses";
 import { products } from "./mocks/products";
 
+/**
+ * @swagger
+ * /products/{productId}:
+ *   get:
+ *     summary: Retrieve a product by ID
+ *     parameters:
+ *       - in: path
+ *         name: productId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The product ID
+ *     responses:
+ *       200:
+ *         description: A single product
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                   description: The product ID.
+ *                   example: "1"
+ *                 name:
+ *                   type: string
+ *                   description: The product name.
+ *                   example: "Product 1"
+ *                 price:
+ *                   type: number
+ *                   description: The product price.
+ *                   example: 100
+ *       404:
+ *         description: Product not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Product not found"
+ */
+
 export const handler = async (event: {
   pathParameters: { productId: string };
 }) => {
--- backend/product-service/get-products-list.ts
+++ backend/product-service/get-products-list.ts
@@ -1,6 +1,34 @@
 import { httpResponse } from "./common/http-responses";
-import { products as mockProducts } from "./mocks/products";
+import { products } from "./mocks/products";
 
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Retrieve a list of products
+ *     responses:
+ *       200:
+ *         description: A list of products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     description: The product ID.
+ *                     example: "1"
+ *                   name:
+ *                     type: string
+ *                     description: The product name.
+ *                     example: "Product 1"
+ *                   price:
+ *                     type: number
+ *                     description: The product price.
+ *                     example: 100
+ */
 export const handler = async () => {
-  return httpResponse(200, mockProducts);
+  return httpResponse(200, products);
 };
