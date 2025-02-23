e671a4a83d29cdf7b047d29252475164704ddfc0
Ihor Osadchyi <ihor_osadchyi@epam.com>
2025-02-23 16:14:55 +0200

    feat: move API_PATHS to .env

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
-  order: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
-  import: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://3xt0fmn54d.execute-api.eu-central-1.amazonaws.com/dev",
+  product: process.env.API_BASE_PATH,
+  order: process.env.API_BASE_PATH,
+  import: process.env.API_BASE_PATH,
+  bff: process.env.API_BASE_PATH,
+  cart: process.env.API_BASE_PATH,
 };
 
 export default API_PATHS;
