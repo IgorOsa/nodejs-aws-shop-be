e86bc8cc0b405eac1ee37b8c3114e12f4bffd162
Ihor <igorosa.com@gmail.com>
2025-03-08 21:41:50 +0200

    cover new lambdas with unit tests

--- backend/src/import-service/importFileParser.ts
+++ backend/src/import-service/importFileParser.ts
@@ -6,27 +6,35 @@ import * as csv from "csv-parser";
 const s3Client = new S3Client({ region: process.env.AWS_REGION });
 
 export const importFileParser: S3Handler = async (event) => {
-  const bucket = event.Records[0].s3.bucket.name;
-  const key = event.Records[0].s3.object.key;
+  try {
+    const bucket = event.Records[0].s3.bucket.name;
+    const key = event.Records[0].s3.object.key;
 
-  const params = {
-    Bucket: bucket,
-    Key: key,
-  };
+    const params = {
+      Bucket: bucket,
+      Key: key,
+    };
 
-  const command = new GetObjectCommand(params);
-  const response = await s3Client.send(command);
-  const s3Stream = response.Body as Readable;
+    const command = new GetObjectCommand(params);
+    const response = await s3Client.send(command);
+    const s3Stream = response.Body as Readable;
 
-  s3Stream
-    .pipe(csv())
-    .on("data", (data) => {
-      console.log("Record:", data);
-    })
-    .on("end", () => {
-      console.log("CSV file successfully processed");
-    })
-    .on("error", (error) => {
-      console.error("Error processing CSV file:", error);
+    await new Promise((resolve, reject) => {
+      s3Stream
+        .pipe(csv())
+        .on("data", (data) => {
+          console.log("Record:", data);
+        })
+        .on("end", () => {
+          resolve("CSV file successfully processed");
+        })
+        .on("error", (error) => {
+          reject(error);
+        });
     });
+
+    console.log("CSV file successfully processed");
+  } catch (error) {
+    console.error("Error processing CSV file:", error);
+  }
 };
